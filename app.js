d4a200c9dc98cf8a03e2b9ec5aa37d3642ffacf7
kirat112 <harkiratsingh1128@gmail.com>
2024-11-14 23:45:41 +0530

    change

--- app.js
+++ app.js
@@ -1,32 +1,4 @@
-// const exp = require('constants');
-// const express = require('express');
-// const path = require('path');
-// const app = express();
-// const port = 3000;
-
-// const server = require('http').createServer(app);
-// const io = require('socket.io')(server);
-
-// app.set('view engine', 'ejs');
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// io.on("connection", (socket) => {
-//   socket.on("send-location", (data)=>{
-//     io.emit("receive-location", {id: socket.id, ...data});
-//   });
-//   socket.on("disconnect", () => {
-//     io.emit("user-disconnected", {id: socket.id});
-//   });
-// });
-
-// app.get('/', (req, res) => {
-//   res.render('index');
-// }); 
-
-// server.listen(port, () => {
-//   console.log(`Example app listening at http://localhost:${port}`);
-// });
-
+const exp = require('constants');
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -39,21 +11,18 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 io.on("connection", (socket) => {
-  // Listen for location updates from clients and broadcast to all
-  socket.on("send-location", (data) => {
-    io.emit("receive-location", { id: socket.id, ...data });
+  socket.on("send-location", (data)=>{
+    io.emit("receive-location", {id: socket.id, ...data});
   });
-
-  // Notify clients when a user disconnects
   socket.on("disconnect", () => {
-    io.emit("user-disconnected", { id: socket.id });
+    io.emit("user-disconnected", {id: socket.id});
   });
 });
 
 app.get('/', (req, res) => {
   res.render('index');
-});
+}); 
 
 server.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
--- public/js/script.js
+++ public/js/script.js
@@ -1,50 +1,5 @@
-// const socket = io();
-
-// if (navigator.geolocation) {
-//   navigator.geolocation.watchPosition(
-//     (position) => {
-//       const { latitude, longitude } = position.coords;
-//       socket.emit("send-location", { latitude, longitude });
-//     },
-//     (error) => {
-//       console.error(error);
-//     },
-//     {
-//       enableHighAccuracy: true,
-//       timeout: 5000,
-//       maximumAge: 0,
-//     }
-//   );
-// }
-
-// const map = L.map("map").setView([0, 0], 16);
-// const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
-// L.tileLayer(tileUrl, { attribution: "Kirat's Map" }).addTo(map);
-
-// const markers = {};
-
-// socket.on("receive-location", (data) => {
-//   const { id, latitude, longitude } = data;
-//   map.setView([latitude, longitude], 16);
-//   if (markers[id]) {
-//     markers[id].setLatLng([latitude, longitude]);
-//     console.log(markers[id]);
-//   } else {
-//     markers[id] = L.marker([latitude, longitude]).addTo(map);
-//   }
-// });
-
-// socket.on("user-disconnected", (data) => {
-//   const { id } = data;
-//   if(markers[id]){
-//     map.removeLayer(markers[id]);
-//     delete markers[id];
-//   }
-// });
-
 const socket = io();
 
-// Geolocation function to track and send location
 if (navigator.geolocation) {
   navigator.geolocation.watchPosition(
     (position) => {
@@ -52,7 +7,7 @@ if (navigator.geolocation) {
       socket.emit("send-location", { latitude, longitude });
     },
     (error) => {
-      console.error("Geolocation error:", error);
+      console.error(error);
     },
     {
       enableHighAccuracy: true,
@@ -62,33 +17,26 @@ if (navigator.geolocation) {
   );
 }
 
-// Initialize map
 const map = L.map("map").setView([0, 0], 16);
 const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
-L.tileLayer(tileUrl, { attribution: "Map Attribution" }).addTo(map);
+L.tileLayer(tileUrl, { attribution: "Kirat's Map" }).addTo(map);
 
-// Track markers by user ID
 const markers = {};
 
-// Listen for location updates and display markers
 socket.on("receive-location", (data) => {
   const { id, latitude, longitude } = data;
-
-  // If marker already exists, update it; otherwise, create a new one
+  map.setView([latitude, longitude], 16);
   if (markers[id]) {
     markers[id].setLatLng([latitude, longitude]);
+    console.log(markers[id]);
   } else {
     markers[id] = L.marker([latitude, longitude]).addTo(map);
   }
-
-  // Set map view to the latest location received
-  map.setView([latitude, longitude], 16);
 });
 
-// Remove marker when a user disconnects
 socket.on("user-disconnected", (data) => {
   const { id } = data;
-  if (markers[id]) {
+  if(markers[id]){
     map.removeLayer(markers[id]);
     delete markers[id];
   }
